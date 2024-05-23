ce98f47f12f7f6758be0e03978e34a1162eca9e2
rivka-yuliia <juliacreadora@gmail.com>
2024-05-23 18:24:21 +0300

    create item model for job item

--- naukri/items.py
+++ naukri/items.py
@@ -1,12 +1,8 @@
-# Define here the models for your scraped items
-#
-# See documentation in:
-# https://docs.scrapy.org/en/latest/topics/items.html
-
 import scrapy
 
 
-class NaukriItem(scrapy.Item):
-    # define the fields for your item here like:
-    # name = scrapy.Field()
-    pass
+class JobItem(scrapy.Item):
+    title = scrapy.Field()
+    company = scrapy.Field()
+    description = scrapy.Field()
+    date = scrapy.Field()
