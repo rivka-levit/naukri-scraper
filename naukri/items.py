132ef2980aa61167c1d989eedd5977ffb1c4d4d0
rivka-yuliia <juliacreadora@gmail.com>
2024-05-23 18:44:36 +0300

    spider code

--- naukri/items.py
+++ naukri/items.py
@@ -5,4 +5,5 @@ class JobItem(scrapy.Item):
     title = scrapy.Field()
     company = scrapy.Field()
     description = scrapy.Field()
+    location = scrapy.Field()
     date = scrapy.Field()
--- naukri/spiders/remote_jobs.py
+++ naukri/spiders/remote_jobs.py
@@ -1,10 +1,32 @@
 import scrapy
+import json
+
+from scrapy.loader import ItemLoader
+
+from naukri.items import JobItem
 
 
 class RemoteJobsSpider(scrapy.Spider):
     name = "remote_jobs"
     allowed_domains = ["naukri.com"]
-    start_urls = ["https://www.naukri.com/remote-jobs"]
+    start_urls = ["https://www.naukri.com/jobapi/v3/search?noOfResults="
+                  "20&urlType=search_by_keyword&searchType=adv&keyword="
+                  "remote&pageNo=1&seoKey=remote-jobs&src=discovery_"
+                  "trendingWdgt_homepage_srch&latLong=31.046051_34.851612"]
 
     def parse(self, response, **kwargs):
-        pass
+        payload = json.loads(response.body)
+
+        for job in payload['jobDetails']:
+            i = ItemLoader(item=JobItem())
+            i.add_value('title', job['title'])
+            i.add_value('company', job['companyName'])
+            i.add_value('description', job['jobDescription'])
+            i.add_value(
+                'location',
+                [x['label'] for x in job['placeholders']
+                 if x['type'] == 'location']
+            )
+            i.add_value('data', job['createdDate'])
+
+            yield i.load_item()
