db9b94795a5bf1052f6f28e88f080cd6e283f038
rivka-yuliia <juliacreadora@gmail.com>
2024-05-24 15:00:51 +0300

    pagination

--- naukri/spiders/remote_jobs.py
+++ naukri/spiders/remote_jobs.py
@@ -5,6 +5,8 @@ from scrapy.loader import ItemLoader
 
 from naukri.items import JobItem
 
+from math import ceil
+
 
 class RemoteJobsSpider(scrapy.Spider):
     name = "remote_jobs"
@@ -13,9 +15,14 @@ class RemoteJobsSpider(scrapy.Spider):
                   "20&urlType=search_by_keyword&searchType=adv&keyword="
                   "remote&pageNo=1&seoKey=remote-jobs&src=discovery_"
                   "trendingWdgt_homepage_srch&latLong=31.046051_34.851612"]
+    page = 1
 
     def parse(self, response, **kwargs):
         payload = json.loads(response.body)
+
+        total_jobs = payload['noOfJobs']
+        pages_per_page = payload['queryParamMap']['noOfResults']
+        page_qty = ceil(total_jobs / pages_per_page)
 
         for job in payload['jobDetails']:
             i = ItemLoader(item=JobItem(), response=response)
@@ -30,3 +37,12 @@ class RemoteJobsSpider(scrapy.Spider):
             i.add_value('date', job['createdDate'])
 
             yield i.load_item()
+
+        while self.page <= page_qty:
+            self.page += 1
+            base_url = (f"https://www.naukri.com/jobapi/v3/search?noOfResults="
+                        f"20&urlType=search_by_keyword&searchType=adv&keyword="
+                        f"remote&pageNo={self.page}&seoKey=remote-jobs&src=discovery_"
+                        f"trendingWdgt_homepage_srch&latLong=31.046051_34.851612")
+
+            yield response.follow(base_url, callback=self.parse)
